a83abe0d7a97ba9a3be9547b939a2f99a80b83e3
김진석 <rlawls1991@wemakeprice.com>
2021-05-02 16:42:39 +0900

    feat - 자바 자주 사용하는 steam 예시

--- src/main/java/com/example/javaStudy/stream/BasicStream.java
+++ src/main/java/com/example/javaStudy/stream/BasicStream.java
@@ -1,10 +1,11 @@
 package com.example.javaStudy.stream;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
 import java.sql.Array;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -55,7 +56,7 @@ public class BasicStream {
         list2.stream().forEach(System.out::print);
         // 아래 코드는 에러가 발생이 된다.
         // 그 이유는 이미 위에서 forEach를 사용해서 최종연산을 행하였기 때문이다.
-        //int count = list2.stream().count();
+        // int count = list2.stream().count();
 
         System.out.println();
 
@@ -63,7 +64,133 @@ public class BasicStream {
         IntStream intStream = new Random().ints(1,46);
         intStream.distinct().limit(6).sorted()
                 .forEach(i -> System.out.println("i : " + i));
-
     }
 
+
+    /**
+     * 스트림 자르기
+     * limit, skip
+     */
+    public void example1(){
+        IntStream intStream = IntStream.range(1,100);
+        intStream.skip(3).limit(5)
+                .forEach(System.out::print);
+        System.out.println();
+    }
+
+    /**
+     * 스트림의 요소 걸러내기
+     * distinct, filter
+     */
+    public void example2(){
+        // distinct
+        IntStream intStream = IntStream.of(1,1,2,3,4,3,2,2,2,2,2,3,6,7,8,9);
+        intStream.distinct()
+                .forEach(System.out::print);
+        System.out.println();
+
+        // filter
+        IntStream rangeIntStream = IntStream.rangeClosed(1,10);
+        rangeIntStream.filter(i -> i % 2 == 0)
+                .forEach(System.out::print);
+        System.out.println();
+
+        IntStream rangeIntStream2 = IntStream.rangeClosed(1,20);
+        rangeIntStream2.filter(i -> i % 2 == 0 && i > 10)
+                .forEach(i -> System.out.print(" " + i));
+        System.out.println();
+    }
+
+    /**
+     * 스트림 정렬
+     */
+    public void example3(){
+        // 아래 두개는 같은 기능을 나눈 것!!
+        // 전자가 더 쉽게 알 수가 있다.
+        List<String> stringList = Arrays.asList("a", "B", "C", "D", "E", "a", "c", "d");
+        stringList.stream()
+                .sorted(String.CASE_INSENSITIVE_ORDER)
+                .forEach(i -> System.out.print(" "+ i));
+        System.out.println();
+
+        stringList.stream()
+                .sorted((first, second) -> {
+                    int res = first.compareToIgnoreCase(second);
+                    return (res == 0) ? first.compareTo(second) : res;
+                 })
+                .forEach(i -> System.out.print(" "+ i));
+        System.out.println();
+
+
+        // reversed 사용
+        stringList.stream()
+                .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                .forEach(i -> System.out.print(" "+ i));
+        System.out.println();
+
+
+        List<TestVO> list = returnSampleData();
+        list.stream()
+            .sorted(Comparator.comparing(TestVO::getName)
+                .thenComparing(TestVO::getAge)
+                .thenComparing(TestVO::getNickName))
+                .forEach(i -> System.out.println(" "+ i.toString()));
+        System.out.println();
+    }
+
+
+    private List<TestVO> returnSampleData(){
+        List<TestVO> returnValue = new ArrayList<>();
+        TestVO addValue = TestVO.builder()
+                .name("김종국")
+                .nickName("가")
+                .age(3)
+                .build();
+        TestVO addValue2 = TestVO.builder()
+                .name("김종국")
+                .nickName("다")
+                .age(2)
+                .build();
+        TestVO addValue3 = TestVO.builder()
+                .name("송지효")
+                .nickName("나")
+                .age(2)
+                .build();
+        TestVO addValue4 = TestVO.builder()
+                .name("양세형")
+                .nickName("나")
+                .age(10)
+                .build();
+        TestVO addValue5 = TestVO.builder()
+                .name("유재석")
+                .nickName("라")
+                .age(1)
+                .build();
+        TestVO addValue6 = TestVO.builder()
+                .name("양세형")
+                .nickName("마")
+                .age(10)
+                .build();
+
+
+        returnValue.add(addValue);
+        returnValue.add(addValue2);
+        returnValue.add(addValue3);
+        returnValue.add(addValue4);
+        returnValue.add(addValue5);
+        returnValue.add(addValue6);
+
+        return returnValue;
+    }
+
+
+
+    @Data
+    @Builder
+    @ToString
+    private static class TestVO{
+        private String name;
+        private String nickName;
+        private int age;
+    }
 }
