53f57ede8c05f742243f749769e2135a6dc98a11
김진석 <rlawls1991@wemakeprice.com>
2021-05-23 14:55:38 +0900

    test add file

---
+++ src/main/java/com/example/javaStudy/testbranch/gitBranchA.java
@@ -0,0 +1,11 @@
+package com.example.javaStudy.testbranch;
+
+public class gitBranchA {
+
+    public void test(){
+        System.out.println("Test Branch A");
+        System.out.println("Test Branch A");
+        System.out.println("Test Branch A");
+        System.out.println("Test Branch A");
+    }
+}
