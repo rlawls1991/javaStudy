1c76805befc2de600e2a3da4238ae6289640019e
김진석 <rlawls1991@wemakeprice.com>
2021-05-23 15:25:24 +0900

    테스트C

---
+++ src/main/java/com/example/javaStudy/testbranch/gitBranchC.java
@@ -0,0 +1,11 @@
+package com.example.javaStudy.testbranch;
+
+public class gitBranchC {
+    public void testA(){
+        System.out.println("TestBranchB method testA");
+        System.out.println("TestBranchB method testA");
+        System.out.println("TestBranchB method testA");
+        System.out.println("TestBranchB method testA");
+        System.out.println("TestBranchB method testA");
+    }
+}
