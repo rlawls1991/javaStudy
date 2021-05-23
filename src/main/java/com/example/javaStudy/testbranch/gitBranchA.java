252e9a06ff5bb2bb7adfe2e2de4f3ecbfbb790e3
김진석 <rlawls1991@wemakeprice.com>
2021-05-23 14:55:38 +0900

    테스트C
    테스트C
    
    테스트B
    
    테스트C
    
    테스트B
    
    test add file

---
+++ src/main/java/com/example/javaStudy/testbranch/gitBranchA.java
@@ -0,0 +1,38 @@
+package com.example.javaStudy.testbranch;
+
+public class gitBranchA {
+
+    public void test(){
+        System.out.println("Test Branch A");
+        System.out.println("Test Branch A");
+        System.out.println("Test Branch A");
+        System.out.println("Test Branch A");
+    }
+
+    public void testA(){
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+    }
+    
+    public void testB(){
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+    }
+
+    public void testC(){
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+        System.out.println("Test Branch B");
+    }
+}
---
+++ src/main/java/com/example/javaStudy/testbranch/gitBranchB.java
@@ -0,0 +1,11 @@
+package com.example.javaStudy.testbranch;
+
+public class gitBranchB {
+    public void testA(){
+        System.out.println("TestBranchB method testA");
+        System.out.println("TestBranchB method testA");
+        System.out.println("TestBranchB method testA");
+        System.out.println("TestBranchB method testA");
+        System.out.println("TestBranchB method testA");
+    }
+}
